4e86b5d1012a26d0245d5aa3344407ff92535236
Qnkacz <wasik.bartosz@outlook.com>
2020-11-01 10:00:53 +0100

    Naprawiłem znalezienie najlepszego ruchu

--- Tic-Tac-Toe/TicTacToe.cs
+++ Tic-Tac-Toe/TicTacToe.cs
@@ -264,9 +264,9 @@ namespace Tic_Tac_Toe
         public Tuple<int, int> GetBestPlaceFor(int player)
         {
             Dictionary<Tuple<int, int>, int> RankingOfChoices = new Dictionary<Tuple<int, int>, int>();
-            for (int i = 0; i < board.GetUpperBound(0); i++)
+            for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < board.GetUpperBound(1); j++)
+                for (int j = 0; j < 3; j++)
                 {
                     if (board[i, j] == 2) //znalazło wolne miejsce
                     {
